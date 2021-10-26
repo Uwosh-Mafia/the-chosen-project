712c0fc2b13c7ba30e2be762e839b7db358dfafd
C-NASIR <cnasir22.0@gmail.com>
2021-10-26 12:43:45 -0500

    Added three new filders and put the model and controller files inside the folder

---
+++ FamilyFeudGame/Helpers/DBReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class DBReader
+{
+	public String dbFileConnectionPath;
+	public DBController dbController;
+
+	public DBReader(String dbFileConnectionPath, DBController dbController)
+	{
+		this.dbFileConnectionPath = dbFileConnectionPath;
+		this.dbController = dbController;
+	}
+}
---
+++ FamilyFeudGame/Models/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Answer
+{
+	public int id;
+	public String finalAnswer;
+	public int points;
+
+	public Answer(int id, String finalAnswer, int points)
+	{
+		this.id = id;
+		this.finalAnswer = finalAnswer;
+		this.points = points;
+	}
+}
