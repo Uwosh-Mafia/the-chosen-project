904787a03fa4a378f353abcc8d9b20ffb9f3a876
Sam Gerend <samgerend4@gmail.com>
2021-11-02 10:06:43 -0500

    Resolved merge conflicts

--- FamilyFeudGame/Models/Section.cs
+++ FamilyFeudGame/Models/Section.cs
@@ -8,6 +8,9 @@ public class Section
     private List<Question> questions = new();
     public int QuestionsCount { get; set; }
 
+    /**
+     * Creates a section with a given ID and Name. Start question count is 0.
+     */
     public Section(int id, string text)
     {
         Text = text;
@@ -15,6 +18,9 @@ public class Section
         QuestionsCount = 0;
     }
 
+    /**
+     * Gets a list of Questions from the given section
+     */
     public List<Question> GetQuestions()
     {
         List<Question> questions;
@@ -23,20 +29,29 @@ public class Section
         return questions;
     }
 
-    public Question GetQuestion(int id)
+    /**
+     * Gets a Question from the given section
+     */
+    public Question GetQuestion(int questID)
     {
-        string query = "SELECT * FROM Question WHERE question_id=" + id;
+        string query = "SELECT * FROM Question WHERE question_id=" + questID + " AND section_id = " + ID;
         Question questionToGet = SubmitQuery(query, "question");
         return questionToGet;
     }
 
+    /**
+     * Adds a single Question to the given section
+     */
     public void AddQuestion(Question currQuestion)
     {
-        string query = "UPDATE Question SET question_id = " + currQuestion.Id + ", name = " + currQuestion.Text + ", num_answers = " + currQuestion.AnswersCount + ", section_id = " + Id;
-        QuestionsCount++;
-        SubmitQuery(query);
+        string query = "UPDATE Question SET question_id = " + currQuestion.ID + ", name = " + currQuestion.Name + ", num_answers = " + currQuestion.AnswersCount + ", section_id = " + ID;
+        questionsCount++;
+        SubmitQuery(query, "add question");
     }
 
+    /**
+     * Adds a list of Questions - one by one - to the given section
+     */
     public void AddQuestions(List<Question> currQuestions)
     {
         for (int i = 0; i < currQuestions.Count; i++)
@@ -44,18 +59,23 @@ public class Section
             string query = "UPDATE Question SET question_id = " + currQuestions[i].Id + ", name = " + currQuestions[i].Text + ", num_answers = " + currQuestions[i].AnswersCount + ", section_id = " + Id;
             QuestionsCount++;
         }
-        SubmitQuery(query);
+        SubmitQuery(query, "add question list");
+    }
+    /**
+     * Updates a Question from the given section with the new Question information
+     */
+    public void UpdateQuestion(int questID, Question newQuestion)
+    {
+        string query = "UPDATE Question SET question_id = " + newQuestion.ID + ", name = " + newQuestion.Name + ", num_answers = " + newQuestion.AnswersCount + ", section_id = " + ID + " WHERE question_id = " + questID;
+        SubmitQuery(query, "update question");
     }
 
-    public void UpdateQuestion(int oldId, Question newQuestion)
+    /**
+     * Submits a query to DBController to delete a Question from the this Section
+     */
+    public void DeleteQuestion(int questID)
     {
-        string query = "UPDATE Question SET question_id = " + newQuestion.Id + ", name = " + newQuestion.Text + ", num_answers = " + newQuestion.AnswersCount + ", section_id = " + Id + " WHERE question_id = " + oldId;
-        SubmitQuery(query);
-    }
-
-    public void DeleteQuestion(int id)
-    {
-        string query = "DELETE FROM Question WHERE question_id = " + id;
-        SubmitQuery(query);
+        string query = "DELETE FROM Question WHERE question_id = " + questID + " AND section_id = " + ID;
+        SubmitQuery(query, "update question list");
     }
 }
