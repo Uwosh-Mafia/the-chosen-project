e1a3677c120b5a7c4a141c90b94e715a656397fe
Nasir Muhumed <muhumn02@uwosh.edu>
2021-11-08 16:56:41 -0600

    connected the windows to the data pipe

--- FamilyFeudGame/Windows/SectionSelectionWindow.xaml.cs
+++ FamilyFeudGame/Windows/SectionSelectionWindow.xaml.cs
@@ -22,27 +22,35 @@ namespace FamilyFeudGame
     {
         private Team team1;
         private Team team2;
-        private DBModel model;
-        public DBController controller;
-        public SectionSelectionWindow(Team team1, Team team2)
+        public DBController dBController;
+        public SectionSelectionWindow(Team team1, Team team2, DBController controller)
         {
             InitializeComponent();
             this.team1 = team1;
             this.team2 = team2;
-            model = new();
-            controller = new(model);
-            SectionBox.ItemsSource = controller.GetSections();
+            dBController = controller;
+            populateSelection();
+        }
+
+        public void populateSelection()
+        {
+            List<Section> sections = dBController.GetSections();
+            String[] sectionNames = new string[sections.Count];
+
+            for (int i = 0; i < sections.Count; i++)
+                sectionNames[i] = sections[i].Name;
+
+            SectionBox.ItemsSource = sectionNames;
         }
 
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
-
+            MessageBox.Show("You selected");
         }
 
         private void Next_Click(object sender, RoutedEventArgs e)
         {
-            QuestionSelectionWindow questionSelectionWindow = new(SectionBox.SelectedItem, controller); // Fix me - Object required, didn't allow Section in QuestionSelectionWindow
+            QuestionSelectionWindow questionSelectionWindow = new(SectionBox.SelectedItem, dBController); // Fix me - Object required, didn't allow Section in QuestionSelectionWindow
             questionSelectionWindow.Show();
             Close();
         }
