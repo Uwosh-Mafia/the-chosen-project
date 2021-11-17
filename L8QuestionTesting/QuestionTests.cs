7466add04f9fdf15a858a938fb5dd1a6aada74eb
Sam Gerend <samgerend4@gmail.com>
2021-11-16 20:22:55 -0600

    Created Sam's testing branch

---
+++ L8QuestionTesting/QuestionTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace L8QuestionTesting
+{
+    [TestClass]
+    public class QuestionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
